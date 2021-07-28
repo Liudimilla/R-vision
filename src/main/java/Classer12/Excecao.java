13e2e090d4a5948728d4a2f9f71a2f3a3a657a2f
Liudimilla <42663858+Liudimilla@users.noreply.github.com>
2021-07-28 20:30:53 -0300

    Exercícios Interface e Polimorfismo

---
+++ src/main/java/Classer12/Excecao.java
@@ -0,0 +1,17 @@
+//Al 47
+package Classer12;
+public class Excecao {
+    public static void main(String[] args) {
+
+        try {
+            int[] vetor = new int[4];
+            System.out.println("Antes da exception ");
+
+            vetor[4] = 1; // ERRO para testa exececao
+            System.out.println("Esse texto nao sera impresso ");
+        } catch(ArrayIndexOutOfBoundsException exception) {
+            System.out.println("Excecao ao acessar um indice do vetor que nao existe");
+        }
+        System.out.println("Esse texto sera impresso apos a exception ");
+    }
+}
